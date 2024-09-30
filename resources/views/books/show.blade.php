4a655528a441e0e1a25914ea09b8783b487dc6eb
Munawer-Taj <taj4580@gmail.com>
2024-09-30 16:45:51 +0600

    Layout is added

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,15 +1,6 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
+@extends('layout')
 
-  <body>
-
-    <h1>BOOKSTORE</h1>
+@section('page-content') 
 
     <table class = "table table-striped table-bordered">
         <tr>
@@ -47,7 +38,4 @@
     <br>
     <a href="{{route('books.index',)}}"> Back </a>
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
-</html>
+@endsection
