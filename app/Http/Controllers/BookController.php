060efdfc026b4b44d7549cd1fd38c6d5f26e6a0f
Munawer-Taj <taj4580@gmail.com>
2024-09-30 20:39:28 +0600

    Add new book validation is added

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -30,8 +30,51 @@ class BookController extends Controller
 
     public function store(Request $request) {
 
-        Book::create($request->all());
+        $rules = [
+            'title'     =>'required',
+            'author'    =>'required',
+            'isbn'      =>'required|size:13',
+            'stock'     =>'required|numeric|integer|gte:0',
+            'price'     =>'required|numeric'
+        ];
 
-        return redirect()->route('books.index');
+        $messages = [
+
+            'stock.gte'     => 'The stock must be greater than or equal to 0',
+        ];
+
+    // Option:1 (it will necessary when the attribute don't matches with DB)
+
+        // $book = new Book();
+        // $book->title = $request->title;
+        // $book->author = $request->author;
+        // $book->isbn = $request->isbn;
+        // $book->stock = $request->stock;
+        // $book->price = $request->price;
+        // $book->save();
+
+    // Option:2 (it will necessary when the attribute don't matches with DB)
+        
+        // $data = [
+
+        //     // $book = new Book();
+        // 'title'     => $request->title,
+        // 'author'    => $request->author,
+        // 'isbn'      =>$request->isbn,
+        // 'stock'     =>$request->stock,
+        // 'price'     =>$request->price,
+        // ];
+        // $book = Book::create($data);
+
+        
+        $request->validate($rules);
+        
+        
+        $book = Book::create($request->all());  // Only work if the attributes matches with the DB
+
+        return redirect()->route('books.show', $book->id); // redirect to show page
+
+        // return redirect()->route('books.index');     // redirect to home page
     }
+    
 }
--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -9,27 +9,32 @@
 
         <div class="mb-2">
             <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" name="title">
+            <input type="text" class="form-control" name="title" value="{{old('title')}}">
+            <div> {{$errors->first('title')}} </div>
         </div>
 
         <div class="mb-2">
             <label for="author" class="form-label">Author</label>
-            <input type="text" class="form-control" name="author">
+            <input type="text" class="form-control" name="author" value="{{old('author')}}">
+            <div> {{$errors->first('author')}} </div>
         </div>
 
         <div class="mb-2">
             <label for="isbn" class="form-label">ISBN</label>
-            <input type="text" class="form-control" name="isbn">
+            <input type="text" class="form-control" name="isbn" value="{{old('isbn')}}">
+            <div> {{$errors->first('isbn')}} </div>
         </div> 
         
         <div class="mb-2">
             <label for="stock" class="form-label">Stock</label>
-            <input type="text" class="form-control" name="stock">
+            <input type="text" class="form-control" name="stock" value="{{old('stock')}}">
+            <div> {{$errors->first('stock')}} </div>
         </div> 
 
         <div class="mb-2">
             <label for="price" class="form-label">Price</label>
-            <input type="text" class="form-control" name="price">
+            <input type="text" class="form-control" name="price" value="{{old(key: 'price')}}">
+            <div> {{$errors->first('price')}} </div>
         </div> 
 
         <button type="submit" class="btn btn-primary">Submit</button>
