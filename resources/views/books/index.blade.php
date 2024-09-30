a577970849dfadad2aba8b634e5db1e6681d022e
Munawer-Taj <taj4580@gmail.com>
2024-09-30 16:07:59 +0600

    Adding bootstrap

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,15 +1,17 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+
+  <body>
+
     <h1>BOOKSTORE</h1>
 
-    <table border="1", cellspacing = "0">
+    <table class="table table-striped table-bordered">
         <tr>
             <th> ID </th>
             <th> Title </th>
@@ -27,6 +29,8 @@
         @endforeach
 
     </table>
-    
-</body>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
 </html>
